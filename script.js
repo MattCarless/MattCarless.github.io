828a90c6cd87c919aeb38f5d397e0dc13b778fa0
Sophie Smith <sophiesmith@Sophie-Smiths-MacBook-Pro.local>
2019-07-13 14:55:15 +0100

    condition/temperature & icon displayed on dom, clears on error

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const locationField = document.getElementById("js-locationinput");
 const locationInput = document.getElementById("js-locationsubmit");
 const weatherCondition = document.getElementById("js-weathercondition");
+const temperature = document.getElementById("js-temperature");
+const weatherIcon = document.getElementById("js-weathericon");
 
 const getUserLocation = (event) => {
     event.preventDefault();
@@ -11,7 +13,7 @@ const getUserLocation = (event) => {
 
 
 const getWeatherData = (userLocation) => {
-    fetch("http://api.openweathermap.org/data/2.5/weather?q=" + userLocation + ",UK&APPID=7c5e998268ad415c134ff54444a16be9")
+    fetch("http://api.openweathermap.org/data/2.5/weather?q=" + userLocation + ",UK&units=metric&APPID=7c5e998268ad415c134ff54444a16be9")
         .then(response => {
             if (response.status === 200) {
                 return response.json();
@@ -29,12 +31,20 @@ const getWeatherData = (userLocation) => {
 
 const onApiSuccess = (data) => {
     weatherCondition.innerHTML = data.weather[0].main;
+    temperature.innerHTML = Math.floor(data.main.temp);
+
+    const imageSrc = "http://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png";
+    weatherIcon.innerHTML = "<img src='" + imageSrc + "'>";
     console.log(data);
 };
 
 const onApiError = (error) => {
     console.log(error);
+    weatherCondition.innerHTML = "";
+    temperature.innerHTML = "";
+    weatherIcon.innerHTML = "";
+
 };
 
 
-locationInput.addEventListener ("click", getUserLocation);
+locationInput.addEventListener("click", getUserLocation);
